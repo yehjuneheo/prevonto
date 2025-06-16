50f08074d20aa733d809b168f254e35d4a7ba48b
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-06-16 16:42:03 -0500

    Created the Blood Pressure page

---
+++ Prevonto/Content/BloodPressureView.swift
@@ -0,0 +1,10 @@
+// Blood Pressure page
+import SwiftUI
+
+struct BloodPressureView: View {
+    var body: some View {
+        Text("Blood Pressure page currently in construction!")
+            .font(.title)
+            .foregroundColor(.green)
+    }
+}
